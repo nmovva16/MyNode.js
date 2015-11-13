555435d8e52d2f66c33a885d710f628e4a01308b
nmovva16 <movva.7@wright.edu>
2015-11-13 15:41:35 -0500

    Update MyServer.js

--- MyServer.js
+++ MyServer.js
@@ -1,17 +1,20 @@
+
 var express = require('express'),
+//importing the nishi.js which communicates with the mongodb
     nishi = require('./routes/nishi');
  
 var app = express();
- 
+ //did not use bodyParser as there is no POST operation. 
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
 });
- 
+ //HTTP get operations
 app.get('/dyslexia/:id', nishi.findById);
 app.get('/dyslexiaImage/:image', nishi.displayImage);
 app.get('/nishi/:name', nishi.findByName);
- 
+
+ //app will be listening at port 3000
 app.listen(3000,'0.0.0.0');
 console.log('Listening on port 3000...');
 
